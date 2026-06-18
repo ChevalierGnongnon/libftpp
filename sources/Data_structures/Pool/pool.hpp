9d606573e5b081aba4b9bd8220cd5fb39c8f2784
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-06-18 22:03:41 +0200

    update

--- sources/Data_structures/Pool/pool.hpp
+++ sources/Data_structures/Pool/pool.hpp
@@ -6,31 +6,10 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 00:09:56 by chhoflac          #+#    #+#             */
-/*   Updated: 2026/05/21 00:40:25 by chhoflac         ###   ########.fr       */
+/*   Updated: 2026/06/13 15:00:35 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include <vector>
 
-template<typename TType>
-class Pool{
-    private:
-        std::vector<TType *> storage;
-        std::vector<TType *> available;
-
-    public:
-        class Object{
-            private:
-                TType       *_ptr;
-                Pool<TType> *_pool;
-            public:
-                TType* operator->();
-        };
-        
-        void    resize(const size_t &numberOfObjectStored);
-        template<typename... TArgs> Object acquire(TArgs&&... p_args);
-        
-       
-};
 
